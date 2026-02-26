a680cae4a722d155c62f6ee2e131f3ba3f8f3505
Elisammc <elisammc05@gmail.com>
2026-02-26 08:55:28 +0100

    Revise comments in num_elem.pl for clarity

--- num_elem.pl
+++ num_elem.pl
@@ -3,9 +3,10 @@ num_elem(+Lista, -Resultado)
 	es cierto si Resultado unifica con el número de elementos de Lista.
 	
 Principio de Inducción
-1) num_elem([], ).
-2) num_elem(n-1) -> num_elem(n)
+1) num_elem(n0, 0). n0 en listas es []
+2) num_elem(n-1) -> num_elem(n).
 */
 
 num_elem([],0).
 num_elem([Cabeza|Resto], R2) :- num_elem(Resto,R), R2 is R + 1.
+
